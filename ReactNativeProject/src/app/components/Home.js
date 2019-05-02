7c50b3292006a31fc13462ca43f5bab3554077bd
Rad Nicolas <nicolas.rad@3pillarglobal.com>
2019-05-02 18:10:59 +0300

    Create project structure

---
+++ ReactNativeProject/src/app/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Home extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text> Home Page</Text>
+			</View>
+		)
+	}
+}
+
---
+++ ReactNativeProject/src/app/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export class Login extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text> Login Page</Text>
+			</View>
+		)
+	}
+}
