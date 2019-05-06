1b267d040149e7c743d6bb8e558d9e5986499b27
Rad Nicolas <nicolas.rad@3pillarglobal.com>
2019-05-06 03:01:10 +0300

    Add log out functionality

--- ReactNativeProject/src/app/components/Settings.js
+++ ReactNativeProject/src/app/components/Settings.js
@@ -1,15 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, Button, AsyncStorage } from 'react-native';
 
 export default class Settings extends React.Component {
+	constructor(props) {
+		super(props)
+	}
+
+	_logUserOut = () => {
+		try {
+			AsyncStorage.removeItem('testUser');
+			this.props.navigation.navigate('Home');
+		}
+		catch (error) {
+			alert('Error while logging out', error);
+		}
+	}
+
 	render() {
 		return (
-			<View>
-				<Text>Settings</Text>
-			</View>
+			<SafeAreaView style={styles.container}>
+				<Button
+					onPress={this._logUserOut}
+					title="Log Out"
+					color="#841584"
+					style={styles.logOutButton}
+				/>
+			</SafeAreaView>
 		)
 	}
 }
 
 const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	logOutButton: {
+		marginTop: 60
+	}
 })
